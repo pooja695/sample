dea2d0afb932c3556b97b8b25465aa65817281d5
Arabi_pooja <poojarajshekhar.cs23@bmsce.ac.in>
2025-10-29 01:44:22 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,17 @@
 # sample
+def get_adult_users(users):
+    adults = []
+    for user in users:
+        if user.get("age", 0) > 18:
+            adults.append(user["name"])
+    return adults
+
+
+# Example usage:
+users_list = [
+    {"name": "Akash", "age": 17},
+    {"name": "Riya", "age": 22},
+    {"name": "Manoj", "age": 19},
+]
+
+print(get_adult_users(users_list))  
